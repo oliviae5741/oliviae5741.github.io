87f9a5cbe94a2a9e5811e7db84e93026848cc539
oliviae5741 <43215478+oliviae5741@users.noreply.github.com>
2018-10-04 15:19:57 -0400

    Create 2018-10-04-weekly-reflection-01.m

---
+++ _posts/2018-10-04-weekly-reflection-01.m
@@ -0,0 +1,9 @@
+---
+layout: default
+---
+<h1> Learning blog </h1>
+<p class="meta">{{ page.date | date_to_string }}</p>
+
+<div class="post">
+  {{ This week we learn a lot, ranging from basic HTML all the way to getting into our first project. we were introduced to amtion. We learned the structers in compeuer scicence and how to use it for our   }}
+</div>
